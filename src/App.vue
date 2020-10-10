eb4cf1f1a8db63b7492ce0ebd8045d46c6239989
Kevin <grj@bwg.art>
2020-10-10 14:56:16 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
+  * {
+    margin: 0;
+    padding: 0;
+  }
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+    color: #000;
+    text-decoration: none;
   }
-}
+
+  ul, li {
+    list-style: none;
+  }
+
+  h1, h2, h3, h4 {
+    font-weight: normal;
+  }
+
+  .page {
+    width: 80%;
+    margin: 0 auto;
+    overflow: hidden;
+  }
 </style>
